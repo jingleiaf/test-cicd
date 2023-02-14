daae0a97d5c935731c3f495acdfb8bbf9d54d6b2
jing LEI <jilei-ext@airfrance.fr>
2023-02-14 11:55:09 +0100

    change shell build

--- ci/build_docker_images.sh
+++ ci/build_docker_images.sh
@@ -2,3 +2,5 @@ echo "The value of the variable is: ${dockerFile_dir}"
 echo "The value of the variable is: ${imageName}"
 echo "The value of the variable is: ${imageTag}"
 echo "The value of the variable is: ${imageVersion}"
+
+docker build -t "${imageTag}" "${dockerFile_dir}"
